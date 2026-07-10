9372a05966625584522f0d437448b1a039ba3756
whousel <houselwilliam70@gmail.com>
2026-07-09 23:05:40 -0500

    Added a soil moisture sensor, lightly cleaned code, and updated DevLog.

--- docs/devlog.md
+++ docs/devlog.md
@@ -1,4 +1,20 @@
 # Project-Verdant
 
 ## July 8, 2026
+
 Created GitHub repository, defined successful version 1, and finished project planning. Tomorrow, I plan to relearn circuit design and how to use analog inputs with an Arduino.
+
+
+
+# July 9, 2026
+
+
+
+Developed code and a working prototype that successfully reads the soil moisture percentage based on tested wet and dry values and turns the pump on or off to maintain adequate soil moisture. Currently facing issues with water depth detection due to non-waterproof components. Ran into issues with the water-level sensor displaying seemingly random values that subsided after a 100 ms delay from when it turned on to its measuring point. Successfully coded and added the water level sensor to the prototype circuit.
+
+
+
+
+
+
+
--- software/ProjectCode/ProjectCode.ino
+++ software/ProjectCode/ProjectCode.ino
@@ -1,21 +1,48 @@
-const int SENSOR = A1; //the sensor to read
+const int MOISTURESENSOR = A1; //the sensor to read
+const int WATERLEVELSENSORPIN = A2;
+const int WATERLEVELSENSORPOWER = 3;
 const int PUMP = 2; //the pin connecting to the pump's input
 const int WET = 230; //sensor reading for wet
 const int DRY = 615; //sensor reading for dry
 const int WET_THRESHOLD = 80; 
 const int DRY_THRESHOLD = 20;
+int waterLevel = 0;
+const unsigned long WATERLEVELINTERVAL = 100;
+const unsigned long MOISTURETESTINGINTERVAL = 1000;
+unsigned long waterLevelTimer = 0;
+unsigned long MoistTimer = 0;
+bool lowWater = true;
+const int LOW_WATER_THRESHOLD = 100;
+
  
 void setup() {
   Serial.begin(9600);
-  pinMode(SENSOR, INPUT);
+  pinMode(MOISTURESENSOR, INPUT);
   pinMode(PUMP, OUTPUT);
+  pinMode(WATERLEVELSENSORPIN, INPUT);
+  pinMode(WATERLEVELSENSORPOWER, OUTPUT);
   digitalWrite(PUMP, HIGH); //ensures the pump starts off
+
 }
  
 void loop() {
-  int value = analogRead(SENSOR);
-  //Serial.println(value); //used for soil moisture measurement
-  int pre = map(value, WET, DRY, 100, 0); 
+  unsigned long currentTime = millis();
+  if (currentTime - MoistTimer >= MOISTURETESTINGINTERVAL)
+  {
+    moistureLevel();
+    MoistTimer = currentTime;
+  }
+  if (currentTime - waterLevelTimer >= WATERLEVELINTERVAL)
+  {
+      findWaterLevel();
+      waterLevelTimer = currentTime;
+  }
+
+}
+void moistureLevel()
+{
+  int value = analogRead(MOISTURESENSOR);
+  int moisturePercentage = map(value, WET, DRY, 100, 0); 
   pre = constrain(pre,0,100);
   Serial.print("Moisture Level: ");
   Serial.print(pre);
@@ -32,12 +59,41 @@ void loop() {
     digitalWrite(PUMP, HIGH);
     Serial.println("Pump: OFF");
   }
-  else
+  else if (lowWater == false)
   {
     Serial.println("Soil is dry");
     digitalWrite(PUMP, LOW);
     Serial.println("Pump: ON");
   }
-  delay(1000); //change to millis
-
+  else
+  {
+    Serial.println("Soil is dry, Water tank empty");
+    digitalWrite(PUMP, HIGH);
+    Serial.println("Pump: OFF");
+  }
 }
+void findWaterLevel() {
+  digitalWrite(WATERLEVELSENSORPOWER, HIGH);  // Turn the sensor ON
+  delay(100); //here until i can get a better sensor
+  int val = analogRead(WATERLEVELSENSORPIN);      // Read the analog value form sensor
+  digitalWrite(WATERLEVELSENSORPOWER, LOW);   // Turn the sensor OFF
+  if (val <= 100)
+  {
+    lowWater = true;
+  }
+  else
+  {
+    lowWater = false;
+  }
+  Serial.print("Water Level Reading: ");
+  Serial.println(val);
+  if(lowWater)
+  {
+      Serial.println("Reservoir: Empty");
+  }
+  else
+  {
+      Serial.println("Reservoir: Full");
+  }
+}          
+                    
